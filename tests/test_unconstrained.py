a6a2f328ccd9e58f56d11d462d42b09961c601fc
YaFo <93720099+yafo1948@users.noreply.github.com>
2023-06-14 14:01:13 +0300

    Add files via upload

--- tests/test_unconstrained.py
+++ tests/test_unconstrained.py
@@ -23,15 +23,13 @@ class TestLineSearchMinMethods(unittest.TestCase):
     bfgs_minimizer = LineSearchSolver("BFGS")
     sr1_minimizer = LineSearchSolver("sr1")
     
-    # f, x0, step_len, obj_tol, param_tol, max_iter
-    # func, x0, obj_tol, param_tol, max_iter):
     def test1(self):
         f, ot, pt, iters = Q1_circle, 10e-8, 10e-12, 100
-        self.assertEqual(self.gd_minimzer.minimize(f, self.x0, "wolfe", ot, pt, iters), (0, True))
+        self.assertEqual(self.gd_minimzer.minimize(f, self.x0, ot, pt, iters), (0, True))
         
-        self.newton_minimizer.minimize(f, self.x0, "wolfe", 0.001, 0.001, iters)
-        self.bfgs_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
-        self.sr1_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
+        self.newton_minimizer.minimize(f, self.x0, 0.001, 0.001, iters)
+        self.bfgs_minimizer.minimize(f, self.x0, ot, pt, iters)
+        self.sr1_minimizer.minimize(f, self.x0, ot, pt, iters)
         
         contour(f, self.gd_minimizer.x_hist, self.newton_minimizer.x_hist, self.bfgs_minimizer.x_hist, self.sr1_minimizer.x_hist, [-2, 2], [-2, 2], "$x_{1}^{2} + x_{2}^{2}$")
         plot(self.gd_minimizer.f_x_hist, self.newton_minimizer.f_x_hist, self.bfgs_minimizer.f_x_hist, self.sr1_minimizer.f_x_hist)
@@ -39,10 +37,10 @@ class TestLineSearchMinMethods(unittest.TestCase):
     def test2(self):
         f, ot, pt, iters = Q2_ellipse, 10e-8, 10e-12, 100
         
-        self.gd_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
-        self.newton_minimizer.minimize(f, self.x0, "wolfe", 0.001, 0.001, iters)
-        self.bfgs_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
-        self.sr1_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
+        self.gd_minimizer.minimize(f, self.x0, ot, pt, iters)
+        self.newton_minimizer.minimize(f, self.x0, 0.001, 0.001, iters)
+        self.bfgs_minimizer.minimize(f, self.x0, ot, pt, iters)
+        self.sr1_minimizer.minimize(f, self.x0, ot, pt, iters)
         
         contour(f, self.gd_minimizer.x_hist, self.newton_minimizer.x_hist, self.bfgs_minimizer.x_hist, self.sr1_minimizer.x_hist, [-2, 2], [-2, 2], "$x_{1}^{2} + 100x_{2}^{2}$")
         plot(self.gd_minimizer.f_x_hist, self.newton_minimizer.f_x_hist, self.bfgs_minimizer.f_x_hist, self.sr1_minimizer.f_x_hist)
@@ -50,10 +48,10 @@ class TestLineSearchMinMethods(unittest.TestCase):
     def test3(self):
         f, ot, pt, iters = Q3_rot_ellipse, 10e-8, 10e-12, 100
         
-        self.gd_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
-        self.newton_minimizer.minimize(f, self.x0, "wolfe", 0.001, 0.001, iters)
-        self.bfgs_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
-        self.sr1_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
+        self.gd_minimizer.minimize(f, self.x0, ot, pt, iters)
+        self.newton_minimizer.minimize(f, self.x0, 0.001, 0.001, iters)
+        self.bfgs_minimizer.minimize(f, self.x0, ot, pt, iters)
+        self.sr1_minimizer.minimize(f, self.x0, ot, pt, iters)
         
         contour(f, self.gd_minimizer.x_hist, self.newton_minimizer.x_hist, self.bfgs_minimizer.x_hist, self.sr1_minimizer.x_hist, [-3, 3], [-3, 3], "$100x_{1}^{2} + x_{2}^{2}$ rotated 30 degrees")
         plot(self.gd_minimizer.f_x_hist, self.newton_minimizer.f_x_hist, self.bfgs_minimizer.f_x_hist, self.sr1_minimizer.f_x_hist)
@@ -62,10 +60,10 @@ class TestLineSearchMinMethods(unittest.TestCase):
     def test4(self):
         f, ot, pt, iters = Rosenbrock, 10e-8, 10e-12, 1000
         
-        self.gd_minimizer.minimize(f, self.x0_rosen, "wolfe", ot, pt, iters)
-        self.newton_minimizer.minimize(f, self.x0_rosen, "wolfe", 0.001, 0.001, iters)
-        self.bfgs_minimizer.minimize(f, self.x0_rosen, "wolfe", ot, pt, iters)
-        self.sr1_minimizer.minimize(f, self.x0_rosen, "wolfe", ot, pt, iters)
+        self.gd_minimizer.minimize(f, self.x0_rosen, ot, pt, iters)
+        self.newton_minimizer.minimize(f, self.x0_rosen, 0.001, 0.001, iters)
+        self.bfgs_minimizer.minimize(f, self.x0_rosen, ot, pt, iters)
+        self.sr1_minimizer.minimize(f, self.x0_rosen, ot, pt, iters)
         
         contour(f, self.gd_minimizer.x_hist, self.newton_minimizer.x_hist, self.bfgs_minimizer.x_hist, self.sr1_minimizer.x_hist, [-1.5, 1.5], [-0.5, 2.1], "Rosenbrock 2 degree")
         plot(self.gd_minimizer.f_x_hist, self.newton_minimizer.f_x_hist, self.bfgs_minimizer.f_x_hist, self.sr1_minimizer.f_x_hist)
@@ -74,7 +72,7 @@ class TestLineSearchMinMethods(unittest.TestCase):
     def test5(self):
         f, ot, pt, iters = linear, 10e-8, 10e-12, 100
         
-        self.gd_minimizer.minimize(f, self.x0, "wolfe", ot, pt, iters)
+        self.gd_minimizer.minimize(f, self.x0, ot, pt, iters)
         
         contour(f, self.gd_minimizer.x_hist, [], [], [], [-200, 2], [-200, 2], "$a^{T}x$")
         plot(self.gd_minimizer.f_x_hist, [], [], [])
@@ -83,10 +81,10 @@ class TestLineSearchMinMethods(unittest.TestCase):
     def test6(self):
         f, ot, pt, iters = exp_tri, 10e-8, 10e-12, 100
         
-        self.gd_minimizer.minimize(f, self.x0_rosen, "wolfe", ot, pt, iters)
-        self.newton_minimizer.minimize(f, self.x0_rosen, "wolfe", 0.001, 0.001, iters)
-        self.bfgs_minimizer.minimize(f, self.x0_rosen, "wolfe", ot, pt, iters)
-        self.sr1_minimizer.minimize(f, self.x0_rosen, "wolfe", ot, pt, iters)
+        self.gd_minimizer.minimize(f, self.x0_rosen, ot, pt, iters)
+        self.newton_minimizer.minimize(f, self.x0_rosen, 0.001, 0.001, iters)
+        self.bfgs_minimizer.minimize(f, self.x0_rosen, ot, pt, iters)
+        self.sr1_minimizer.minimize(f, self.x0_rosen, ot, pt, iters)
         
         contour(f, self.gd_minimizer.x_hist, self.newton_minimizer.x_hist, self.bfgs_minimizer.x_hist, self.sr1_minimizer.x_hist, [-3, 3], [-3, 3], "exp(x1 -3x2 - 0.1) + exp(x1 - 3x2 - 0.1) + exp(-x1 - 0.1)")
         plot(self.gd_minimizer.f_x_hist, self.newton_minimizer.f_x_hist, self.bfgs_minimizer.f_x_hist, self.sr1_minimizer.f_x_hist)
